4193dcd21872ba3183cc5f4e08c0a8a17753085d
Derin Dickson Vaca <101202290+derinvc23@users.noreply.github.com>
2023-07-07 09:27:10 -0400

    Add file update view pivot

--- invoice_report.py
+++ invoice_report.py
@@ -18,7 +18,7 @@
 #
 ##############################################################################
 
-from odoo import fields, models, api, _
+from odoo import fields, models,api, _
 
 
 class Invoicelinecount(models.Model):
@@ -47,57 +47,44 @@ class Invoicelinecount(models.Model):
     count_line = fields.Integer(string="Count", compute="get_count_as", store=False)
     count_line1 = fields.Integer(string="Count1", default=1)
 
-
 class AccountInvoiceReport(models.Model):
     _inherit = 'account.invoice.report'
 
     amount_taxes = fields.Float(string='Impuestos', readonly=True)
     amount_totals = fields.Float(string='Total con Impuestos', readonly=True)
     number = fields.Char(string='Nro Factura')
-    costo = fields.Float(string='Costo de Venta', readonly=True)
-    cantidad_actual = fields.Float(string='Stock Actual', readonly=True)
+#    brand_name = fields.Char(string='Marca de producto', readonly=True)
+#    product_brand_id = fields.Many2one('product.brand', string='Marca de producto', readonly=True)
+
+#    _depends = {
+#        'product.brand':['name'],
+#    }
+
+    raw_value = fields.Many2one('product.category', string='Categoria Padre', compute='_get_parent_category', store=True)
+
+    @api.depends('categ_id')
+    def _get_parent_category(self):
+        for record in self:
+            record.raw_value = self.get_base_parent(record.categ_id)
+
+    def get_base_parent(self, categ_id):
+        if not categ_id.parent_id:
+            return categ_id.id
+        else:
+            return self.get_base_parent(categ_id.parent_id)
 
     def _select(self):
         return super(AccountInvoiceReport, self)._select() \
-               + ", sub.amount_taxes as amount_taxes, sub.amount_totals as amount_totals, sub.number, sub.costo, sub.cantidad_actual"
+               + ", sub.amount_taxes as amount_taxes, sub.amount_totals as amount_totals, sub.number"
 
     def _sub_select(self):
         return super(AccountInvoiceReport, self)._sub_select() \
-               + """,CASE WHEN t0.tax_id is not null then
-                        sum(((ail.price_unit * ail.quantity) - (invoice_type.sign::numeric * ail.quantity/(u.factor * u2.factor)*ail.price_unit * ail.discount/100::numeric))*0.13)
-                    ELSE
-                        0
-                    END AS amount_taxes
-                    ,((ail.price_unit * ail.quantity)-(((ail.price_unit * ail.quantity) * ail.discount) / 100)) as amount_totals, ai.number,
-                    sum(s1.total)                                                                       as costo,
-                    sum(sq.total_qty)                                                                   as cantidad_actual
-                    """
-
-    def _from(self):
-        res = super(AccountInvoiceReport, self)._from()
-        return res + """
-        left join account_invoice_line_tax t0 on t0.invoice_line_id = ail.id
-        left join (select foo.origin, foo.product_id, sum(foo.total) as total
-                          from (
-                                   select sm.product_id,
-                                          sm.origin,
-                                          sm.product_uom_qty,
-                                          sm.price_unit,
-                                          (sm.product_uom_qty * sm.price_unit) as total
-                                   from stock_move sm
-                                   where sm.state in ('done')
-                               ) as foo
-                          group by foo.origin, foo.product_id
-                          order by foo.origin) as s1 on s1.product_id = ail.product_id and s1.origin = ai.origin
-        left join (select sq.product_id,
-                         sum(sq.qty) as total_qty
-                  from stock_quant sq
-                           inner join stock_location sl on sq.location_id = sl.id
-                  where sl.usage in ('internal')
-                  group by sq.product_id
-                  order by sq.product_id) as sq on sq.product_id = ail.product_id
-        """
-
+               + ",((ai.amount_tax)*(1)) AS amount_taxes, " \
+                 " ((ail.price_unit * ail.quantity)-(((ail.price_unit * ail.quantity) * ail.discount) / 100)) as amount_totals, ai.number"
+#    def _sub_select(self):
+#       return super(AccountInvoiceReport, self)._sub_select() \
+#             + ",((ai.amount_tax)*(1)) AS amount_taxes, " \
+#              "(ail.price_unit)*(ail.quantity) as amount_totals ,ai.number"
 
     def _group_by(self):
-        return super(AccountInvoiceReport, self)._group_by() + ", ai.number, t0.tax_id"
+        return super(AccountInvoiceReport, self)._group_by() + ", ai.number"
